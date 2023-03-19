8aa0eb1b45e3fb2d058bf4e149dd7c37a9849e10
aabds <wogus8880@gmail.com>
2023-03-19 23:25:50 +0900

    api적용중

---
+++ exammap/app/src/main/java/com/example/exammap/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.exammap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+    }
+
+}
